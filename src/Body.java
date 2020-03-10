5791daf78bb21247ff3c5c9077a1fc30564dce2b
Oliver Wöhrer <e11907563@student.tuwien.ac.at>
2020-03-10 11:31:40 +0100

    First test commit

--- src/Body.java
+++ src/Body.java
@@ -1,9 +1,93 @@
-
-// Body represents a body or object in space with 3D-position, velocity and mass.
-// This class will be used by the executable class 'Space'. A body moves through space according to
-// its inertia and - if specified - an additional force exerted on it (for example the
-// gravitational force of another mass).
+/**
+ * Body represents a body or object in space with 3D-position, velocity and mass.
+ * This class will be used by the executable class 'Space'. A body moves through space according to
+ * its inertia and - if specified - an additional force exerted on it (for example the
+ * gravitational force of another mass).
+ */
 public class Body {
     //TODO: class definition.
+    private double mass;                                // mass of body in kg
+    private float[] position = new float[3];            // position in space via XYZ coordinates in meters
+    public float[] velocity = new float[3];             // velocity vector via XYZ coordinates in m/s
+
+    /**
+     * Setter Method for setting the mass of an object
+     * @param massArg
+     */
+    public void setMass(double massArg) {
+        this.mass = massArg;
+    }
+
+    /**
+     * Setter Method for positioning the body in space
+     * @param x coordinate in space in meters
+     * @param y coordinate in space in meters
+     * @param z coordinate in space in meters
+     */
+    public void setPosition(int x, int y, int z) {
+        position[0] = x;
+        position[1] = y;
+        position[2] = z;
+    }
+
+    /**
+     * Setter Method for setting the current velocity of the body
+     * @param vx velocity in x direction
+     * @param vy velocity in y direction
+     * @param vz velocity in z direction
+     */
+    public void setVelocity(float vx, float vy, float vz) {
+        velocity[0] = vx;
+        velocity[1] = vy;
+        velocity[2] = vz;
+    }
+
+    /**
+     * returns the distance of body to origin in z direction
+     * @return absolute position in z direction
+     */
+    public float getHeight() {
+        return this.position[2];
+    }
+
+    /**
+     * returns the absolute body position in space [X, Y, Z]
+     * @return reference for [X, Y, Z] array
+     */
+    public float[] getPosition() {
+        return this.position;
+    }
+
+    public double gravitationalForce() {
+        // kg*m/sec² ... F = mass * acc
+        return Space.G*Space.MASS_OF_EARTH*this.mass/(Space.RADIUS_OF_EARTH*Space.RADIUS_OF_EARTH);
+    }
+
+    /**
+     * move() does a linear body movement in space in the direction of the current
+     * velocity of the body. One movement takes exactly 1 sec to perform.
+     */
+    public void move() {
+        for (byte i=0; i < 3; i++) {
+            this.position[i] += this.velocity[i];
+        }
+    }
+
+    /**
+     * This method changes the current velocity according to the given acceleration forces before
+     * performing a body movement in space in the direction of the new resulting velocity vector.
+     * The resulting vector is the sum of the new force vector passed in the arguments and the current
+     * velocity vector. To match the units of the vectors, the forces need to be divided by the mass
+     * of the body: force = mass * acceleration: [N] = [kg] * [m/s^2]
+     * @param fx acceleration force in x direction
+     * @param fy acceleration force in y direction
+     * @param fz acceleration force in z direction
+     */
+    public void move(double fx, double fy, double fz) {
+        this.velocity[0] += (float)(fx / this.mass);  // add current and new directions together
+        this.velocity[1] += (float)(fy / this.mass);
+        this.velocity[2] += (float)(fz / this.mass);
+        move();                     // perform linear movement
+    }
 
 }
--- src/Space.java
+++ src/Space.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 // Space is the actual program (executable class) using objects of class 'Body'.
 public class Space {
@@ -25,11 +26,11 @@ public class Space {
         //Results for the falling ball on the surface of earth are as follows:
         //Height 10m: 2 (sec = number of move(fx,fy,fz) calls)
         //Height 100m: 5 (sec = number of move(fx,fy,fz) calls)
-        /*
+
         Body ball1 = new Body();
+        ball1.setMass(1); // 1 kg
         ball1.setPosition(0,0,100); // 100m height.
         ball1.setVelocity(0,0,0);
-        ball1.setMass(1); // 1 kg
         System.out.println(fallToGround(ball1)); // 5
 
         ball1.setPosition(0,0,10); // 10m height.
@@ -37,27 +38,76 @@ public class Space {
         System.out.println(fallToGround(ball1)); // 2
 
         Body ball2 = new Body();
+        ball2.setMass(15); // 15 kg
         ball2.setPosition(0,0,100); // 100m height.
         ball2.setVelocity(0,0,0);
-        ball2.setMass(15); // 15 kg
-        System.out.println(fallToGround(ball1)); // 5
-        */
-
+        System.out.println(fallToGround(ball2)); // 5
         //Further examples are to be tested (body in empty space, rocket, feather).
 
+        //TODO: implement test cases!
+        //orbital object test case:
+        Body orb = new Body();
+        orb.setMass(2e3);
+        orb.setPosition(0,0,0);
+        orb.setVelocity(1,2,3);   // 3.7m/s initial velocity
+        linearMovement(10, orb);    // move for 10 seconds
+        System.out.println("new orb position: " + Arrays.toString(orb.getPosition()) );
+
+        //rocket test case:
+        Body rocket = new Body();
+        rocket.setMass(2e3);    // 2000kg
+        rocket.setPosition(0,0,0);
+        rocket.setVelocity(0,0,0);   // 0m/s initial velocity at take off
+        acceleratedMovement(30, rocket, 0, 0, 5e6);
+        System.out.println("new rocket position: " + Arrays.toString(rocket.getPosition()) );
+
+        //feather test case:
+        Body feather = new Body();
+        feather.setMass(0.001);
+        feather.setPosition(0, 0, 0);
+        feather.setVelocity(0, 0, 0);
+        //feather.randomMovement();
+        //System.out.println("new feather position: ");
     }
 
     // Returns the number of move(fx,fy,fz) calls needed for 'b' hitting the ground, i.e.,
     // the method reduces the z-coordinate of 'b' until it becomes 0 or negative.
     public static int fallToGround(Body b) {
-
         //TODO: implement recursive method.
-        return 0;
-
+        if (b.getHeight() > 0) {                                // check if body has hit the ground
+            b.move(0, 0, -b.gravitationalForce()); // apply gravitational force to body
+            return  1 + fallToGround(b);                        // body has fallen for one additional second
+        } else {    // body has hit the ground (end recursion)
+            return 0;
+        }
     }
 
     //TODO: Define further methods as needed.
+    /**
+     * moves an object through space for a given number of seconds at the current velocity
+     * @param seconds duration of movement in seconds
+     * @param b pass object to be moved
+     */
+    public static void linearMovement(int seconds, Body b) {
+        if (seconds > 0) {  // recursive step
+            b.move(); // perform linear movement
+            linearMovement(seconds - 1, b); // enter recursion
+        } // else: movement finished
+    }
 
+    public static void acceleratedMovement(int seconds, Body b, double fx, double fy, double fz) {
+        if (seconds > 0) {  // recursive step
+            b.move(fx, fy, fz); // perform accelerated movement
+            acceleratedMovement(seconds-1, b, fx, fy, fz);
+        } // else: movement finished
+    }
+
+    public static void randomMovement(Body b) {
+        double fx = 10*Math.random();
+        double fy = 10*Math.random();
+        double fz = 10*Math.random();
+        // recursive call of acceleratedMovement with new acceleration forces
+    }
 }
 
 
