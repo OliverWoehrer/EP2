fcaa4fd4d4b13bb3f1ee2f3c5104a60d2aef0ed0
Anton Ertl <anton@mips.complang.tuwien.ac.at>
2020-03-27 18:46:24 +0100

    Aufgabenblatt 3

---
+++ src/CelestialSystem.java
@@ -0,0 +1,38 @@
+public class CelestialSystem {
+
+    //TODO: Define variables.
+
+    // Initialises this system as an empty system with a name.
+    public CelestialSystem(String name) {
+        //TODO: implement constructor.
+    }
+
+    // Adds 'body' to the list of bodies of the system if the list does not already contain a
+    // body with the same name as 'body', otherwise does not change the object state. The method
+    // returns 'true' if the list was changed as a result of the call and 'false' otherwise.
+    public boolean add(CelestialBody body) {
+        //TODO: implement method.
+        return false;
+    }
+
+    // Returns the 'body' with the index 'i'. The body that was first added to the list has the
+    // index 0, the body that was most recently added to the list has the largest index (size()-1).
+    public CelestialBody get(int i) {
+        //TODO: implement method.
+        return null;
+    }
+
+    // Returns the body with the specified name or 'null' if no such body exits in the list.
+    public CelestialBody get(String name) {
+        //TODO: implement method.
+        return null;
+    }
+
+    // returns the number of entries of the list.
+    public int size() {
+        //TODO: implement method.
+        return -1;
+    }
+
+    //TODO: Define additional class(es) implementing the linked list (either here or outside class).
+}
